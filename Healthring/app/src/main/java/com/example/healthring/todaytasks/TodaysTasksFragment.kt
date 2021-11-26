b862f8a6e9f29720e38e309161115531cf40aab6
Nick Schleiff <nschleiff@sd7.net>
2021-11-26 11:32:54 -0800

    Created todaystasks fragment

---
+++ Healthring/app/src/main/java/com/example/healthring/todaytasks/TodaysTasksFragment.kt
@@ -0,0 +1,23 @@
+package com.example.healthring.todaytasks
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.healthring.R
+
+class TodaysTasksFragment : Fragment() {
+
+    private lateinit var viewModel: TodaysTasksViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel = ViewModelProvider(this).get(TodaysTasksViewModel::class.java)
+
+        return inflater.inflate(R.layout.todays_tasks_fragment, container, false)
+    }
+}
