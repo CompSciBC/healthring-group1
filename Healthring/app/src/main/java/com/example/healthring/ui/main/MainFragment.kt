8b85501ef84c9b8d0af76a1ce51bdbe7ff8fdfc2
noahlaxton <noahlaxton@gmail.com>
2021-11-27 12:05:16 -0800

    UI Revision edits

--- Healthring/app/src/main/java/com/example/healthring/ui/main/MainFragment.kt
+++ Healthring/app/src/main/java/com/example/healthring/ui/main/MainFragment.kt
@@ -20,7 +20,7 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        return inflater.inflate(R.layout.todays_task_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
