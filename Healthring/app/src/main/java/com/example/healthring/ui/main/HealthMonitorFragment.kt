38e075d8f4050ae0d029562cc28728908b3c09f4
Filbert-code <alexlingfungfilbert@gmail.com>
2021-11-27 15:39:25 -0800

    Added databinding functionality

--- Healthring/app/src/main/java/com/example/healthring/ui/main/HealthMonitorFragment.kt
+++ Healthring/app/src/main/java/com/example/healthring/ui/main/HealthMonitorFragment.kt
@@ -1,7 +1,41 @@
 package com.example.healthring.ui.main
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.healthring.R
+import com.example.healthring.databinding.HealthMonitorFragmentBinding
 
 class HealthMonitorFragment : Fragment(R.layout.health_monitor_fragment){
+
+    private var binding : HealthMonitorFragmentBinding? = null
+    private val mainViewModel : MainViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentBinding = HealthMonitorFragmentBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            // pass instance of the viewmodel and HealthMonitorFragment
+            healthMonitorFragment = this@HealthMonitorFragment
+            viewModel = mainViewModel
+        }
+    }
+
+    fun goToTaskFragment() {
+        findNavController().navigate(R.id.action_healthMonitorFragment_to_todaysTasksFragment2)
+    }
 }
