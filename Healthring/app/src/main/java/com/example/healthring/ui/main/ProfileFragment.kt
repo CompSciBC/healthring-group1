7b0ede8024bf690117be6536d4aeefc91a6324e9
Milan Gurung <milan.gurung@bellevuecollege.edu>
2021-11-27 23:45:54 -0800

    Added databindings to health moniter fragment and added Profile Fragment in UI main

--- Healthring/app/src/main/java/com/example/healthring/ui/main/ProfileFragment.kt
+++ Healthring/app/src/main/java/com/example/healthring/ui/main/ProfileFragment.kt
@@ -8,12 +8,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.healthring.R
-import com.example.healthring.databinding.HealthMonitorFragmentBinding
+import com.example.healthring.databinding.ProfileFragmentBinding
 
+class ProfileFragment : Fragment(R.layout.profile_fragment){
 
-class HealthMonitorFragment : Fragment(R.layout.health_monitor_fragment){
-
-    private var binding : HealthMonitorFragmentBinding? = null
+    private var binding : ProfileFragmentBinding? = null
     private val mainViewModel : MainViewModel by viewModels()
 
     override fun onCreateView(
@@ -21,7 +20,7 @@ class HealthMonitorFragment : Fragment(R.layout.health_monitor_fragment){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val fragmentBinding = HealthMonitorFragmentBinding.inflate(inflater, container, false)
+        val fragmentBinding = ProfileFragmentBinding.inflate(inflater, container, false)
         binding = fragmentBinding
         return fragmentBinding.root
     }
@@ -30,22 +29,14 @@ class HealthMonitorFragment : Fragment(R.layout.health_monitor_fragment){
         super.onViewCreated(view, savedInstanceState)
         binding?.apply {
             lifecycleOwner = viewLifecycleOwner
-            // pass instance of the viewmodel and HealthMonitorFragment
-            healthMonitorFragment = this@HealthMonitorFragment
+            // pass instance of the viewmodel and ProfileFragment
+            profileFragment = this@ProfileFragment
             viewModel = mainViewModel
         }
     }
 
-    fun goToFitnessFragment() {
-        findNavController().navigate(R.id.action_healthMonitorFragment_to_fitnessTrackerFragment)
-    }
-
-    fun goToTaskFragment() {
-        findNavController().navigate(R.id.action_healthMonitorFragment_to_todaysTasksFragment2)
-    }
-
-    fun goToProfileFragment() {
-        findNavController().navigate(R.id.action_healthMonitorFragment_to_profileFragment)
+    fun goToHealthMonitorFragment() {
+        findNavController().navigate(R.id.action_profileFragment_to_healthMonitorFragment)
     }
 
 }
