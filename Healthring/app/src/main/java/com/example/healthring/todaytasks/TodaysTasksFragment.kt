f993c48da63d117295c61cb2803f2e35205e116f
noahlaxton <noahlaxton@gmail.com>
2021-11-28 16:10:38 -0800

    TodaysTasks navigation update

--- Healthring/app/src/main/java/com/example/healthring/todaytasks/TodaysTasksFragment.kt
+++ Healthring/app/src/main/java/com/example/healthring/todaytasks/TodaysTasksFragment.kt
@@ -1,23 +1,51 @@
 package com.example.healthring.todaytasks
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.healthring.R
+import com.example.healthring.databinding.TodaysTaskFragmentBinding
+import com.example.healthring.ui.main.MainViewModel
 
-class TodaysTasksFragment : Fragment() {
+class TodaysTasksFragment : Fragment(R.layout.todays_task_fragment) {
 
-    private lateinit var viewModel: TodaysTasksViewModel
+    private var binding : TodaysTaskFragmentBinding? = null
+    private val mainViewModel : MainViewModel by viewModels()
+    // private val todaysTasksViewModel : TodaysTasksViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel = ViewModelProvider(this).get(TodaysTasksViewModel::class.java)
-
-        return inflater.inflate(R.layout.todays_task_fragment, container, false)
+        val fragmentBinding = TodaysTaskFragmentBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            todaysTasksFragment = this@TodaysTasksFragment
+            viewModel = MainViewModel()
+        }
+    }
+
+    fun goToHealthMonitorFragment() {
+        findNavController().navigate(R.id.action_todaysTasksFragment2_to_healthMonitorFragment2)
+    }
+
+    fun goToFitnessFragment() {
+        findNavController().navigate(R.id.action_todaysTasksFragment2_to_fitnessTrackerFragment)
+    }
+
+    fun goToProfileFragment() {
+        findNavController().navigate(R.id.action_todaysTasksFragment2_to_profileFragment)
+    }
+
 }
