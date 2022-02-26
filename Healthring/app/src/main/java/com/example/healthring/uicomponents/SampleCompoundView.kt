6d56d88c4864fe5a452fbd594d014635bd44e98e
Nick Schleiff <nschleiff@sd7.net>
2022-02-26 08:59:26 -0800

    Implemented sample compound view

---
+++ Healthring/app/src/main/java/com/example/healthring/uicomponents/SampleCompoundView.kt
@@ -0,0 +1,36 @@
+package com.example.healthring.uicomponents
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.healthring.R
+
+class SampleCompoundView : ConstraintLayout {
+
+    private val button : Button;
+    private val textView: TextView;
+
+    //Constructors
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    )
+
+    init{
+        inflate(context, R.layout.sample_compound_view, this)
+
+        button = findViewById(R.id.sampleButton)
+        textView = findViewById(R.id.sampleText)
+
+        button.setOnClickListener {
+            Log.i("SampleCompoundView", "I did something!")
+            android.widget.Toast.makeText(context, "Boom shaka laka", android.widget.Toast.LENGTH_SHORT).show()
+        }
+    }
+}
