29755b7adc24c319ad88f39c9e0a74526c625f6e
Filbert-code <alexlingfungfilbert@gmail.com>
2022-02-28 20:25:43 -0800

    Added HealthMonitorViewModel to get sensors to change color

--- Healthring/app/src/main/java/com/example/healthring/healthmonitor/HealthMonitorFragment.kt
+++ Healthring/app/src/main/java/com/example/healthring/healthmonitor/HealthMonitorFragment.kt
@@ -1,14 +1,20 @@
 package com.example.healthring.healthmonitor
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
 import com.amazonaws.mobile.auth.core.internal.util.ThreadUtils.runOnUiThread
@@ -23,6 +29,7 @@ class HealthMonitorFragment : Fragment(R.layout.health_monitor_fragment){
 
     private var binding : HealthMonitorFragmentBinding? = null
     private val dataVM: DataViewModel by activityViewModels()
+    private val viewModel: HealthMonitorViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,6 +48,7 @@ class HealthMonitorFragment : Fragment(R.layout.health_monitor_fragment){
             // pass instance of the viewmodel and HealthMonitorFragment
             healthMonitorFragment = this@HealthMonitorFragment
             dataViewModel = dataVM
+            healthMonitorViewModel = viewModel
         }
 
         // grabs the latest sensor data from the database every time delta
@@ -75,5 +83,15 @@ class HealthMonitorFragment : Fragment(R.layout.health_monitor_fragment){
     fun goToGraphFragment() {
         dataVM.getReportData(Sensors.H_RATE)
         findNavController().navigate(R.id.action_healthMonitorFragment_to_graphFragment)
+    }
+
+    fun updateHeartRateColor() {
+        if (dataVM.heart_rate.value!! < 100) {
+            viewModel.hRateColor.value = ContextCompat.getDrawable(requireContext(), R.drawable.blue_border)
+        } else if ((dataVM.heart_rate.value!! < 130)) {
+            viewModel.hRateColor.value = ContextCompat.getDrawable(requireContext(), R.drawable.green_border)
+        } else {
+            viewModel.hRateColor.value = ContextCompat.getDrawable(requireContext(), R.drawable.amber_border)
+        }
     }
 }
