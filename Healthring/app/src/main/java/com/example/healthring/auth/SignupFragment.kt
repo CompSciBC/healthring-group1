161885f16c529efd0348f0110560199c70c3c6ba
noahlaxton <noahlaxton@gmail.com>
2022-05-01 19:58:31 -0700

    confirmation fragment 3

--- Healthring/app/src/main/java/com/example/healthring/auth/SignupFragment.kt
+++ Healthring/app/src/main/java/com/example/healthring/auth/SignupFragment.kt
@@ -13,8 +13,9 @@ import com.amplifyframework.auth.options.AuthSignUpOptions
 import com.amplifyframework.core.Amplify
 import com.example.healthring.R
 import com.example.healthring.databinding.SignupFragmentBinding
+import com.google.android.material.snackbar.Snackbar
 
-class SignupFragment : Fragment(R.layout.fitness_tracker_fragment) {
+class SignupFragment : Fragment() {
 
     private var binding : SignupFragmentBinding? = null
     private val authViewModel : AuthViewModel by viewModels()
@@ -46,33 +47,55 @@ class SignupFragment : Fragment(R.layout.fitness_tracker_fragment) {
         findNavController().navigate(R.id.action_signupFragment_to_loginFragment)
     }
 
-    fun registerNewUser() {
+    fun registerNewUserAndGoToConfirmationPage() {
+        val succeeded = registerNewUser()
+
+        if (succeeded) {
+            goToConfirmationFragment()
+        } else {
+            Snackbar.make(binding?.root!!, "Email or either password entered was not correct.", Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    fun registerNewUser(): Boolean {
         // TODO: Add a try-catch to handle assertion errors for when email/pwd is null
+        var succeeded = false
         val options = AuthSignUpOptions.builder()
             .userAttribute(AuthUserAttributeKey.email(), authViewModel.email.value!!)
             .build()
         Amplify.Auth.signUp(authViewModel.email.value!!, authViewModel.password.value!!, options,
             // Sign up succeeded, go to sign up fragment
-            { Log.i("AuthQuickStart", "Sign up succeeded: $it") },
+            {
+                Log.i("AuthQuickStart", "Sign up succeeded: $it")
+                succeeded = true
+            },
             // Implement that tells the user that sign up fails.
             // use a toast
-            { Log.e ("AuthQuickStart", "Sign up failed", it) }
+            {
+                Log.e ("AuthQuickStart", "Sign up failed", it)
+            }
         )
+
+        return succeeded
     }
 
-    fun confirmNewUser() {
-        Log.i("Amplify", "confirmation code: ${authViewModel.confirmCode.value.toString()}")
-        Amplify.Auth.confirmSignUp(
-            authViewModel.email.value!!, authViewModel.confirmCode.value.toString(),
-            { result ->
-                if (result.isSignUpComplete) {
-                    Log.i("AuthQuickstart", "Confirm signUp succeeded")
-                } else {
-                    Log.i("AuthQuickstart","Confirm sign up not complete")
-                }
-            },
-            { Log.e("AuthQuickstart", "Failed to confirm sign up", it) }
-        )
+    fun goToConfirmationFragment() {
+        findNavController().navigate(R.id.action_signupFragment_to_confirmationFragment)
     }
+
+//    fun confirmNewUser() {
+//        Log.i("Amplify", "confirmation code: ${authViewModel.confirmCode.value.toString()}")
+//        Amplify.Auth.confirmSignUp(
+//            authViewModel.email.value!!, authViewModel.confirmCode.value.toString(),
+//            { result ->
+//                if (result.isSignUpComplete) {
+//                    Log.i("AuthQuickstart", "Confirm signUp succeeded")
+//                } else {
+//                    Log.i("AuthQuickstart","Confirm sign up not complete")
+//                }
+//            },
+//            { Log.e("AuthQuickstart", "Failed to confirm sign up", it) }
+//        )
+//    }
 
 }
