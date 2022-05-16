f87dfc8cbc38999582dcb5d52e7d063fe59c1314
noahlaxton <noahlaxton@gmail.com>
2022-05-16 15:00:57 -0700

    email focuslistener

--- Healthring/app/src/main/java/com/example/healthring/auth/LoginFragment.kt
+++ Healthring/app/src/main/java/com/example/healthring/auth/LoginFragment.kt
@@ -2,9 +2,12 @@ package com.example.healthring.auth
 
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.getBinding
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -18,7 +21,7 @@ import kotlinx.coroutines.*
 import okhttp3.internal.wait
 import java.lang.NullPointerException
 
-class LoginFragment : Fragment(R.layout.fitness_tracker_fragment) {
+class LoginFragment : Fragment() {
 
     private var binding: LoginFragmentBinding? = null
     private val authViewModel: AuthViewModel by viewModels()
@@ -42,6 +45,7 @@ class LoginFragment : Fragment(R.layout.fitness_tracker_fragment) {
             loginFragment = this@LoginFragment
             loginVM = loginViewModel
             viewModel = authViewModel
+
         }
 
         Amplify.Auth.fetchAuthSession(
@@ -56,6 +60,13 @@ class LoginFragment : Fragment(R.layout.fitness_tracker_fragment) {
             },
             { Log.e("AuthQuickStart", "Failed to fetch session", it) }
         )
+
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        emailFocusListener()
     }
 
     private fun signInSuccessful(session: AWSCognitoAuthSession) {
@@ -93,7 +104,11 @@ class LoginFragment : Fragment(R.layout.fitness_tracker_fragment) {
             },
             {
                 Log.e("AuthQuickstart", "Failed to sign in", it)
-                Snackbar.make(binding?.root!!, "Username or password entered was not correct.", Snackbar.LENGTH_LONG).show()
+                Snackbar.make(
+                    binding?.root!!,
+                    "Username or password entered was not correct.",
+                    Snackbar.LENGTH_LONG
+                ).show()
             }
         )
     }
@@ -111,7 +126,11 @@ class LoginFragment : Fragment(R.layout.fitness_tracker_fragment) {
             },
             {
                 Log.e("AuthQuickstart", "Failed to sign in", it)
-                Snackbar.make(binding?.root!!, "Username or password entered was not correct.", Snackbar.LENGTH_LONG).show()
+                Snackbar.make(
+                    binding?.root!!,
+                    "Username or password entered was not correct.",
+                    Snackbar.LENGTH_LONG
+                ).show()
             }
         )
     }
@@ -131,5 +150,50 @@ class LoginFragment : Fragment(R.layout.fitness_tracker_fragment) {
     fun goToSignupFragment() {
         findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
     }
+
+    // ------------------------------------------------------------------------------------
+    // Validate Edit Text
+    private fun emailFocusListener() {
+        binding?.loginEmail?.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                binding?.loginEmail?.helperText = validEmail()
+            }
+        }
+    }
+
+    public fun validEmail(): String? {
+        val emailText = authViewModel.email.value.toString()
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
+            return "Invalid Email Address"
+        }
+        return null
+    }
+
+//    private fun passwordFocusListener() {
+//
+//        binding.passwordEditText.setOnFocusChangeListener { _, focused ->
+//            if (!focused) {
+//                bindingTest.passwordContainer.helperText = validPassword()
+//            }
+//        }
+//
+//    }
+//
+//    private fun validPassword(): String? {
+//        val passwordText = bindingTest.passwordEditText.text.toString()
+//        if (passwordText.length < 8) {
+//            return "Minimum 8 Character Password"
+//        }
+//        if (!passwordText.matches(".*[A-Z].*".toRegex())) {
+//            return "Must Contain 1 Upper-case Character"
+//        }
+//        if (!passwordText.matches(".*[a-z].*".toRegex())) {
+//            return "Must Contain 1 Lower-case Character"
+//        }
+//        if (!passwordText.matches(".*[@#\$%^&+=].*".toRegex())) {
+//            return "Must Contain 1 Special Character (@#\$%^&+=)"
+//        }
+//        return null
+//    }
 
 }
